003effda313b4e31420f6eb63fddaf43361b13c3
IsmaelCorreia <160427953+ismael-ds-correia@users.noreply.github.com>
2026-04-14 13:42:14 -0300

    feat: implementa modo interativo para geração de texto

--- simple_llm.py
+++ simple_llm.py
@@ -1,4 +1,5 @@
 from transformers import pipeline, AutoTokenizer
+from transformers import AutoModelForCausalLM
 
 def create_simple_llm():
     model_name = "distilgpt2"
@@ -6,10 +7,21 @@ def create_simple_llm():
 
     return generator
 
-generator = create_simple_llm()
+def generator_text(generator, prompt, max_length = 1000):
+    result = generator(prompt, max_length=max_length, do_sample=True, temperature=0.7)
 
-prompt = "once upon a time"
+    return result[0]["generated_text"]
 
-generated_text = generator(prompt, max_length=100, num_return_sequences=1, truncation=True)
+def interactive_demo():
+    generator = create_simple_llm()
 
-print(generated_text[0]['generated_text'])
+    while True:
+        prompt = input("\nDigite: ")
+        if prompt.lower() == 'quit':
+            break
+
+        response = generator_text(generator, prompt)
+        print('\nResposta: ')
+        print(response)
+
+interactive_demo()
